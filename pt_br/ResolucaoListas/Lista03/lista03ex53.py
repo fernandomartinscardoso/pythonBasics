b4d8e5c731a812da8b26d8b9254d474b27da93ee
Fernando Martins Cardoso <fernandomartinscardoso@gmail.com>
2023-01-13 23:13:46 +0100

    Adding exercise lista03ex53.py

---
+++ pt_br/ResolucaoListas/Lista03/lista03ex53.py
@@ -0,0 +1,9 @@
+"""Gabarito da Lista 3 - Comandos de Repetição
+   Exercício 53
+   Programador: Fernando Martins Cardoso
+"""
+print('###'*23)
+print('#                         Triângulo de Floyd                        #'.upper())
+print('#    Linha 1: 1; linha 2: 2 3; linha 3: 4 5 6; linha 4: 7 8 9 10    #')
+print('###'*23)
+
