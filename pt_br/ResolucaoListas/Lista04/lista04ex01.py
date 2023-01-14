ca5a457899053e6a4187bd5620c6209d97d6af5a
Fernando Martins Cardoso <fernandomartinscardoso@gmail.com>
2023-01-14 23:11:39 +0100

    Adding exercise Lista04 folder

---
+++ pt_br/ResolucaoListas/Lista04/lista04ex01.py
@@ -0,0 +1,12 @@
+"""Gabarito da Lista 3 - Comandos de Repetição
+   Exercício 01
+   Programador: Fernando Martins Cardoso
+"""
+
+print('###'*14)
+print('# EXIBINDO OS 5 PRIMEIROS MÚLTIPLOS DE 3 #'.upper())
+print('###'*14)
+
+print('\nOs cinco primeiros múltiplos de 3 são:')
+for i in range(1, 6):
+    print(3*i)
