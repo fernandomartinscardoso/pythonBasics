9138624087d672c0024f7f839a3ac254f545cc75
Fernando Martins Cardoso <fernandomartinscardoso@gmail.com>
2024-06-02 19:06:38 +0200

    Changing matplotlib config to fix plot from TKinter graphic interface

--- gui/dataInterface/app/plotdata.py
+++ gui/dataInterface/app/plotdata.py
@@ -3,12 +3,14 @@ from  matplotlib import pyplot as plt
 import matplotlib
 import seaborn as sns
 
+matplotlib.use('Qt5Agg')
+
 def regression_plot(filename, xlabel, ylabel):
     # create the dataframe using the csv file upload
     df = pd.read_csv(filename)
     # Temp, Year and Rain(fall)
     # How we set width, height using matplotlib settings
-    sns.set(rc={'figure.figsize':(12,6)})
+    sns.set_theme(rc={'figure.figsize':(12,6)})
     sns.regplot(x=xlabel, y=ylabel, data=df)
     # regression line shows a possible positive correlation - as temp increases so does rainfall.
     plt.show() 
