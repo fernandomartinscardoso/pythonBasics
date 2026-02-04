c6a3bbe720f2131535164c1544a2fda8be8e0227
fernandomartinscardoso <85852657+fernandomartinscardoso@users.noreply.github.com>
2026-02-04 19:14:03 +0100

    Refactor word display logic and adjust timer
    Refactor word display functions and update timer duration.

--- 100DaysOfPython/journey/day031/main.py
+++ 100DaysOfPython/journey/day031/main.py
@@ -6,24 +6,34 @@ from tkinter import *
 from tkinter import messagebox
 from random import randint, choice, shuffle
 import pandas
-import pyperclip
-import json
 
 BACKGROUND_COLOR = "#B1DDC6"
 FONT_NAME = "Ariel"
-TIME_TO_GUESS = 5
+TIME_TO_GUESS = 5000
+key = None
+timer = None
 
 # Creating the German-to-English dictionary:
 data = pandas.read_csv("data/german_words.csv")
 data_german = data.German.to_list()
 data_english = data.English.to_list()
-de_en_dict = {data_german[i]: data_english[i] for i in range(len(data_german))}
+numbers = [j for j in range(3000)]
 
-# Count down function:
-timer = None
-def count_down(count):
-    global timer
-    timer = window.after(1000, count_down, count-1)
+# Main functions
+def show_german():
+    global key
+    key = choice(numbers)
+    canvas.itemconfig(word_text, text=data_german[key])
+
+def show_english():
+    global key
+    canvas.itemconfig(card_image, image=card_back_image)
+    canvas.itemconfig(language_text, text="English", fill="white")
+    canvas.itemconfig(word_text, text=data_english[key], fill="white")
+
+def show_word():
+    show_german()
+    window.after(TIME_TO_GUESS, show_english)
 
 # Word checking function
 def wrong_word():
@@ -31,6 +41,7 @@ def wrong_word():
 
 def right_word():
     pass
+
 # Window setup
 window = Tk()
 window.title("Flashy")
@@ -39,7 +50,10 @@ window.config(pady=50, padx=50, bg=BACKGROUND_COLOR)
 # Canvas setup
 canvas = Canvas(width=900, height=626, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_image = PhotoImage(file="images/card_front.png")
-canvas.create_image(450, 273, image=card_front_image)
+card_back_image = PhotoImage(file="images/card_back.png")
+card_image = canvas.create_image(450, 273, image=card_front_image)
+language_text = canvas.create_text(450, 170, text="Deutsch", fill="black", font=(FONT_NAME, 40, "italic"))
+word_text = canvas.create_text(450, 300, text=key, fill="black", font=(FONT_NAME, 60, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
 
 # Buttons
@@ -51,15 +65,6 @@ right_button_image = PhotoImage(file="images/right.png")
 wrong_word_button = Button(image=right_button_image, highlightthickness=0, command=right_word)
 wrong_word_button.grid(column=1, row=1)
 
-# Labels
-def show_german():
-    key = choice(data_german)
-    language_label = Label(text="Deutsch", font=(FONT_NAME, 40, "italic"), bg="white")
-    language_label.place(x=350, y=100)
-
-    word_label = Label(text=key, font=(FONT_NAME, 60, "bold"), bg="white")
-    word_label.place(x=350-10*len(key), y=213)
-
-show_german()
+show_word()
 
 window.mainloop()
